b92ff8a30538a06395b259d7a548723697460255
Valet78 <valet78@mail.ru>
2024-08-28 18:09:51 +0300

    first commit

---
+++ include/25.4-1.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <iostream>
+#include <locale>
+#include <ctime>
+
+
+// Глобальные переменные
+
+
+// Функции
---
+++ src/main_25-1.cpp
@@ -0,0 +1,17 @@
+// Задание 23.4.1. 
+#include "../include/25.4-1.h"
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    system("chcp 1251");
+    system ("cls");
+
+    std::wcout << L"Проверка кода вывода!" << std::endl;
+
+
+    system("pause");
+    return 0;
+}
+
+// *************************************
+
